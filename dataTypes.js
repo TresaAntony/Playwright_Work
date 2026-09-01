867b994c0e0476dd6196603322a9f0c8bfb0f921
TresaAntony <tresa.ch5@gmail.com>
2026-09-01 16:15:47 -0400

    DataTypes Day2
    Week 1 - Day 2 - Data Types Class work

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+/**DATATYPES */
+
+//string
+var firstName ="Tresa" // 'single quote // double quote '' back tick
+console.log("FirstName : "+firstName)
+console.log("Datatype of FirstName : "+typeof firstName)
+
+//string
+var companyName ="TestLeaf" // 'single quote // double quote '' back tick
+console.log("CompanyName : "+companyName)
+console.log("Datatype of companyName : "+typeof companyName)
+
+//mobileNumber
+var mobileNumber =80895757347 // numbers
+console.log("MobileNumber : "+mobileNumber)
+console.log("Datatype of mobileNumber : "+typeof mobileNumber)
+
+//isAutomation
+var isAutomation = true // boolean
+console.log("IsAutomation : "+isAutomation)
+console.log("Datatype of isAutomation : "+typeof isAutomation)
+
+//hasPlayWright
+var hasPlayWright// boolean
+console.log("hasPlayWright : "+hasPlayWright)
+console.log("Datatype of hasPlayWright : "+typeof hasPlayWright)
